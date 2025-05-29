9f8e9081c3d055b84a3ae76839dda6a80f523aa7
Rohan <Kumarohan999@gmail.com>
2025-05-29 19:00:00 +0000

    lec 5

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS Code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    *
+    4)      //Code Readibility should be high
+
+
+console.log("Strange")
+
+let name = "Rohan"
+let age  = 21
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// String
+// Boolean => True/False
+// null => standalone value // object
+// undefined => value not assigned to variable //datatype
+// symbol => unique
+
+
+// Object 
+console.log(typeof undefined);
+console.log(typeof null);
