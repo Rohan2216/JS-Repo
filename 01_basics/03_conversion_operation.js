a7a633f98c4f0f4ac5d62bf8b7d6981b82a441cc
Rohan <Kumarohan999@gmail.com>
2025-06-19 12:45:01 +0000

    lecture 6 minimal changes

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -7,9 +7,9 @@ let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
-
+//Converting in Numbers there must be
 // "33" => 33
-// "33abc" => NaN
+// "33abc" => NaN(not a Number)
 //  true => 1; false => 0
 
 let isLoggedIn = "hitesh"
@@ -24,14 +24,14 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,7 +44,7 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
